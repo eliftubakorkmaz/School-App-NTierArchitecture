10620890d2e71bb2a817b9fba61e83520b80ffef
etubas <etubasavli@gmail.com>
2024-04-08 17:16:08 +0300

    Add project files.

---
+++ NTierArchitecture.DataAccess/Repositories/StudentRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NTierArchitecture.DataAccess.Context;
+using NTierArchitecture.Entities.Models;
+using System.Linq.Expressions;
+
+namespace NTierArchitecture.DataAccess.Repositories;
+
+public sealed class StudentRepository
+    (ApplicationDbContext context) : IStudentRepository
+{
+    public void Create(Student student)
+    {
+        context.Add(student);
+        context.SaveChanges();
+    }
+
+    public IQueryable<Student> GetAll()
+    {
+        return context.Students.AsNoTracking().AsQueryable();
+    }
+
+    public Student? GetStudentById(Guid studentId)
+    {
+        return context.Students.Find(studentId);
+    }
+
+    public void Update(Student student)
+    {
+        context.SaveChanges();
+    }
+    public void DeleteById(Guid Id)
+    {
+        Student? student = GetStudentById(Id);
+        if (student is not null)
+        {
+            student.IsDeleted = true;
+            context.SaveChanges();
+        }
+    }
+
+    public int GetNewStudentNumber()
+    {
+        int lastStudentNumber = 0;
+        if (context.Students.Any()) // Öğrenci koleksiyonunda herhangi bir öğe var mı kontrol et
+        {
+            lastStudentNumber = context.Students.Max(p => p.StudentNumber);
+        }
+        else
+        {
+            lastStudentNumber = 100; // Varsayılan bir başlangıç değeri
+        }
+
+        if (lastStudentNumber <= 100) lastStudentNumber = 100;
+        lastStudentNumber++;
+
+
+        return lastStudentNumber;
+    }
+
+    public bool Any(Expression<Func<Student, bool>> predicate)
+    {
+        return context.Students.AsNoTracking().Any(predicate);
+    }
+}
---
+++ NTierArchitecture.WebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NTierArchitecture.Business;
+using NTierArchitecture.Entities.DTOs;
+
+namespace NTierArchitecture.WebAPI.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+[Authorize(AuthenticationSchemes = "Bearer")]
+public sealed class StudentsController(IStudentService studentService) : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Create(CreateStudentDto request)
+    {
+        string message = studentService.Create(request);
+        return Ok(new { message = message });
+    }
+
+    [HttpPost]
+    public IActionResult Update(UpdateStudentDto request)
+    {
+        string message = studentService.Update(request);
+        return Ok(new { message = message });
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult DeleteById(Guid id)
+    {
+        string message = studentService.DeleteById(id);
+        return Ok(new { message = message });
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var response = studentService.GetAll();
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetAllByClassRoomId(PaginationRequestDto request)
+    {
+        var response = await studentService.GetAllByClassRoomIdAsync(request);
+        return Ok(response);
+    }
+}
